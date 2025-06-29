b511f0dadf04c2267034aa8ded3ea168072c6693
hasan-mavlonov <hmavlanov79@gmail.com>
2025-06-29 22:23:55 +0500

    changed secret keys

--- Tracky/settings.py
+++ Tracky/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tracky-d764.onrender.com']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 
 # Application definition
 
