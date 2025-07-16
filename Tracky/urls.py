6ba48471d01de34447c207f230e9480a5e3dbf59
hasan-mavlonov <hmavlanov79@gmail.com>
2025-07-16 15:55:47 +0500

    added ladning page translation

--- Tracky/urls.py
+++ Tracky/urls.py
@@ -14,15 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
 from django.contrib import admin
 from django.urls import path, include
 from .views import BaseView, LandingView
 
 urlpatterns = [
-    path('dashboard/', BaseView, name='base_view'),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('shops/', include('shops.urls')),
     path('sales/', include('sales.urls')),
-    path('', LandingView, name='landing_view'),
+    path('set_language/', set_language, name='set_language'),  # ✅ Add this
 ]
+
+# i18n URLs (if you're using translation on the landing/dashboard)
+urlpatterns += i18n_patterns(
+    path('', LandingView, name='landing_view'),
+    path('dashboard/', BaseView, name='base_view'),
+)
